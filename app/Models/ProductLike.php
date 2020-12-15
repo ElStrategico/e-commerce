77f6a50821ac899074448b14095f5d5a2dd3fc55
El Strategico <strategico.dev@gmail.com>
2020-12-16 03:17:49 +0800

    Added ProductLike model

---
+++ app/Models/ProductLike.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProductLike extends Model
+{
+    protected $fillable = [
+        'product_id', 'user_id'
+    ];
+
+    public static function likeExists(Product $product, User $user)
+    {
+        $productLike = self::where('product_id', '=', $product->id)->
+                             where('user_id', '=', $user->id)->
+                             first();
+
+        return $productLike != null;
+    }
+
+    public static function like(Product $product, User $user)
+    {
+        return self::create([
+            'product_id' => $product->id,
+            'user_id'    => $user->id
+        ]);
+    }
+}
