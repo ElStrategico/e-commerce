e2e77d44b7989b0d97817f1e7fc1a11693f1148c
El Strategico <strategico.dev@gmail.com>
2020-12-15 17:04:00 +0800

    Refactoring

--- app/Http/Controllers/API/v1/AuthController.php
+++ app/Http/Controllers/API/v1/AuthController.php
@@ -2,13 +2,55 @@
 
 namespace App\Http\Controllers\API\v1;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class AuthController extends Controller
 {
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function login(Request $request)
     {
-        return 'Login';
+        $credentials = $request->only(['email', 'password']);
+        $token = auth()->attempt($credentials);
+
+        if(!$token)
+        {
+            return response()->json([
+                'error' => 'Unauthorized'
+            ], 401);
+        }
+
+        return $this->responseToken($token);
+    }
+
+    /**
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function me()
+    {
+        return response()->json(auth()->user());
+    }
+
+    /**
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function refresh()
+    {
+        return $this->responseToken(auth()->refresh());
+    }
+
+    /**
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout()
+    {
+        auth()->logout();
+
+        return response()->json([
+            'message' => 'Successfully logged out'
+        ]);
     }
 }
