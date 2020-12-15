de5b672cf855a7bc30f32191ab28e392825ef9ad
El Strategico <strategico.dev@gmail.com>
2020-12-15 20:20:12 +0800

    Added RuleController

---
+++ app/Http/Controllers/API/v1/RuleController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\API\v1;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class RuleController extends Controller
+{
+    public function index()
+    {
+        $user = User::find(auth()->id());
+
+        return $user->rules;
+    }
+}
