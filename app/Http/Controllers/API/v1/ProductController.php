2859ae5aa25835528991093cda0f8550969c08e4
El Strategico <strategico.dev@gmail.com>
2020-12-16 20:33:17 +0800

    Added methods: create, update, delete

--- app/Http/Controllers/API/v1/ProductController.php
+++ app/Http/Controllers/API/v1/ProductController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers\API\v1;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use App\Models\Produce\Product;
 use App\Http\Controllers\Controller;
 use App\Models\Produce\ProductSearch;
 use App\Models\Produce\ProductOptions;
+use App\Http\Requests\CreateProductRequest;
+use App\Http\Requests\UpdateProductRequest;
 
 class ProductController extends Controller
 {
@@ -28,11 +31,48 @@ class ProductController extends Controller
      */
     public function show(Product $product)
     {
+        $user = User::find(auth()->id());
+
         $product->category;
+        $product->models;
         $product->images;
         $product->videos;
 
         $product->increaseViews();
+
+        return $product;
+    }
+
+    /**
+     * @param CreateProductRequest $request
+     * @return Product
+     */
+    public function store(CreateProductRequest $request)
+    {
+        $request->merge(['user_id' => auth()->id()]);
+
+        return Product::create($request->input());
+    }
+
+    /**
+     * @param Product $product
+     * @param UpdateProductRequest $request
+     * @return Product
+     */
+    public function update(Product $product, UpdateProductRequest $request)
+    {
+        $product->update($request->input());
+
+        return $product;
+    }
+
+    /**
+     * @param Product $product
+     * @return Product
+     */
+    public function delete(Product $product)
+    {
+        $product->delete();
 
         return $product;
     }
