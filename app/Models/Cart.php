af85102e707491680e23c0229eb862a20f530756
El Strategico <strategico.dev@gmail.com>
2020-12-16 20:30:20 +0800

    Added Cart model

---
+++ app/Models/Cart.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Cart extends Model
+{
+    const ARCHIVE = 1;
+
+    use HasFactory;
+}
