720aa77fc96bfa70befe2753fbd5babce1492aeb
El Strategico <strategico.dev@gmail.com>
2020-12-17 21:53:46 +0800

    Refactoring

--- app/Http/Controllers/API/v1/CartController.php
+++ app/Http/Controllers/API/v1/CartController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\API\v1;
 use App\Models\User;
 use App\Models\Cart;
 use Illuminate\Http\Request;
+use App\Models\Produce\Product;
 use App\Http\Controllers\Controller;
+use App\Models\Produce\ProductModel;
 
 class CartController extends Controller
 {
@@ -13,6 +15,66 @@ class CartController extends Controller
     {
         $user = User::find(auth()->id());
 
-        return $user->carts()->where('archive', '=', !Cart::ARCHIVE)->get();
+        return $user->cartsProducts();
+    }
+
+    public function total()
+    {
+        $user = User::find(auth()->id());
+
+        return Cart::total($user);
+    }
+
+    /**
+     * @param Request $request
+     * @param Product $product
+     * @param ProductModel $productModel
+     * @return Cart
+     */
+    public function store(Request $request, Product $product, ProductModel $productModel = null)
+    {
+        $user = User::find(auth()->id());
+        $amount = (int)$request->input('amount');
+
+        return Cart::add($amount, $user, $product, $productModel);
+    }
+
+    /**
+     * @param Cart $cart
+     * @return Cart
+     */
+    public function show(Cart $cart)
+    {
+        return $cart;
+    }
+
+    /**
+     * @param Cart $cart
+     * @param Request $request
+     * @return Cart
+     */
+    public function update(Cart $cart, Request $request)
+    {
+        $cart->update($request->input());
+
+        return $cart;
+    }
+
+    /**
+     * @param Cart $cart
+     * @return Cart
+     */
+    public function delete(Cart $cart)
+    {
+        $cart->archive();
+
+        return $cart;
+    }
+
+    public function clear()
+    {
+        $user = User::find(auth()->id());
+
+        return Cart::clear($user);
     }
 }
