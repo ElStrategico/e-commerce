25b3a86d7123285de672cb3ce9feec689121acb6
El Strategico <strategico.dev@gmail.com>
2020-12-21 20:06:25 +0800

    Added method: details

--- app/Models/Produce/ProductModel.php
+++ app/Models/Produce/ProductModel.php
@@ -2,10 +2,16 @@
 
 namespace App\Models\Produce;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class ProductModel extends Model
 {
     use HasFactory;
+
+    public function details()
+    {
+        return $this->belongsToMany(Detail::class)->
+                      select('name', 'description', 'value');
+    }
 }
