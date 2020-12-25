2847b3e746cef9f31e3d1a86f1a8d5001df34eb7
El Strategico <strategico.dev@gmail.com>
2020-12-25 19:16:10 +0800

    Added logging

--- app/Http/Controllers/API/v1/RuleController.php
+++ app/Http/Controllers/API/v1/RuleController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\API\v1;
 
 use App\Models\User;
-use App\Logger\Converter;
 use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\Controller;
 
@@ -12,6 +11,10 @@ class RuleController extends Controller
     public function index()
     {
         $user = User::find(auth()->id());
+
+        Log::info('Show rules', [
+            'user' => $user->id
+        ]);
 
         return $user->rules;
     }
